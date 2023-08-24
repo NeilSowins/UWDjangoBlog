6632010d16ff64c7444bab199380abbe4688ed88
Neil Sowinski <neilsowinski@gmail.com>
2023-08-23 21:02:15 -0700

    completing categories for blogging

--- blogging/models.py
+++ blogging/models.py
@@ -14,11 +14,12 @@ class Post(models.Model):
 
 class Category(models.Model):
 
-    # name
-    # description
-    # posts
-
     name = models.CharField(max_length=128)
-    description = models.CharField(blank=True)
+    description = models.TextField(blank=True)
+    posts = models.ManyToManyField(Post, blank=True, related_name='categories')
 
-    pass
+    class Meta:
+        verbose_name_plural = 'Categories' 
+
+    def __str__(self):
+        return self.name
