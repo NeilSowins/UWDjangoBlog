5d8033264d4be5266af91bd307c2f3ab377ea1dd
Neil Sowinski <neilsowinski@gmail.com>
2023-09-05 21:17:03 -0700

    implementing blogging and polling classes. Blogging detail is not working yet

--- blogging/views.py
+++ blogging/views.py
@@ -2,31 +2,34 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from blogging.models import Post
 from django.template import loader
-
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
-# rewrite our view
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    context = {'posts': posts}
-    return render(request, 'blogging/list.html', context)
+class BloggingListView(ListView):
+    model = Post
+    template_name = 'blogging/list.html'
+    context_object_name = 'posts'
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
 
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+class BloggingDetailView(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
+    context_object_name = 'post'
+    queryset = Post.objects.exclude(published_date__exact=None)
+
+    def get_post(self, queryset=None):
+        try:
+            return queryset.get(pk=self.kwargs.get('post_id'))
+        except Post.DoesNotExist:
+            raise Http404
+
+# def detail_view(request, post_id):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     try:
+#         post = published.get(pk=post_id)
+#     except Post.DoesNotExist:
+#         raise Http404
+#     context = {'post': post}
+#     return render(request, 'blogging/detail.html', context)
