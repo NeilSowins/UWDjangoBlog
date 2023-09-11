9b3e64ab0e7fc3ce932f66d798617af1d1d22b42
Neil Sowinski <neilsowinski@gmail.com>
2023-09-11 13:32:45 -0700

    starting serializer and adding base for rest framework

--- blogging/urls.py
+++ blogging/urls.py
@@ -4,4 +4,6 @@ from blogging.views import BloggingListView, BloggingDetailView
 urlpatterns = [
     path("", BloggingListView.as_view(), name="blog_index"),
     path("posts/<int:pk>/", BloggingDetailView.as_view(), name="blog_detail"),
+    path("api/", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
