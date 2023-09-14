d645108fb9e54bbf31530268c1d0125650b2487e
Neil Sowinski <neilsowinski@gmail.com>
2023-09-13 22:18:00 -0700

    adding serializers for user, categories, posts, updating settings, views and urls

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,5 +1,13 @@
-from django.urls import path
-from blogging.views import BloggingListView, BloggingDetailView
+from django.urls import path, include
+from rest_framework import routers
+from blogging.views import BloggingListView, BloggingDetailView, PostViewSet, CategoryViewSet, UserViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'users', UserViewSet, basename='user-detail')
+
 
 urlpatterns = [
     path("", BloggingListView.as_view(), name="blog_index"),
--- blogging/views.py
+++ blogging/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-from blogging.models import Post
+from blogging.models import Post, Category
 from django.template import loader
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework import permissions
+from blogging.serializers import PostSerializer, CategorySerializer, UserSerializer
 
 
 class BloggingListView(ListView):
@@ -30,3 +34,27 @@ class BloggingDetailView(DetailView):
             return queryset.get(pk=key)
         except Post.DoesNotExist:
             raise Http404
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    '''
+    API endpoint that allows Posts to be viewed or edited.
+    '''
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    '''
+    API endpoint that allows Category to be viewed or edited.
+    '''
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
